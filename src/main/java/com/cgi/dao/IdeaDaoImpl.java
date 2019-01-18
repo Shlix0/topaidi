f9edb57882799646e38830261b1f813a9eddddd8
Xavier <xavier.baffert@gmail.com>
2019-01-18 13:58:31 +0100

    tien

--- src/main/java/com/cgi/dao/IdeaDaoImpl.java
+++ src/main/java/com/cgi/dao/IdeaDaoImpl.java
@@ -129,7 +129,7 @@ public class IdeaDaoImpl implements IdeaDao {
 		List<Idea> ideas = em.createQuery("from Idea i").getResultList();
 		List<Idea> ideasR = new ArrayList<Idea>();
 		for(Idea i : ideas) {
-			if (i.getUsersReport().size() < 0) ideas.add(i);
+			if (i.getUsersReport().size() > 0) ideas.add(i);
 
 		}
 		return ideasR;
